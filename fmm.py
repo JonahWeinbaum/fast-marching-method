f9767dee0d12e1992f8eb3b605b8db8f86b6c0c5
JonahWeinbaum <jonah.r.weinbaum.gr@dartmouth.edu>
2025-12-11 16:59:32 -0500

    Commenting and cleaning -- small optimizations for visualization

--- fmm.py
+++ fmm.py
@@ -2,8 +2,12 @@ import trimesh
 import heapq
 import numpy as np
 from collections import defaultdict
+from geodesic import *
 
 def eikonal_update_triangle(A, B, C, TA, TB):
+    # Given two known eikonal solutions, find the third
+    # by approximating the front as a plane
+    
     a = np.linalg.norm(B - C)
     b = np.linalg.norm(A - C)
     AC = A - C
@@ -15,7 +19,8 @@ def eikonal_update_triangle(A, B, C, TA, TB):
     sin2_theta = 1 - cos_theta**2
 
     fallback = min(TA + b, TB + a)
-    
+
+    # At degeneracy just use Dijkstra
     if cos_theta <= 0.0:
         return fallback
 
@@ -24,19 +29,24 @@ def eikonal_update_triangle(A, B, C, TA, TB):
     coeff_C = b**2 * (u**2 - a**2*sin2_theta)
 
     disc = coeff_B**2 - 4*coeff_A*coeff_C
-
+    
+    # At degeneracy just use Dijkstra
     if disc < 0.0:
         return fallback
 
     sqrt_d = np.sqrt(disc)
-    
+
+    # Solutions to quadratic 
     t1 = (-coeff_B + sqrt_d) / (2*coeff_A)
     t2 = (-coeff_B - sqrt_d) / (2*coeff_A)    
 
     valid = [t for t in (t1, t2) if u < t]
+
+    # At degeneracy just use Dijkstra
     if valid == []:
         return fallback
-    
+
+    # Compute TC using best t value
     t = min(valid)
     if a*cos_theta < (b*(t-u)/t) and \
        (b*(t-u)/t) < a / cos_theta:
@@ -50,35 +60,38 @@ def fast_marching(mesh, source_idx):
     
     N = len(V)
 
+    # Initialize infinite distance for all nodes
+    # except source
     distance = np.full(N, np.inf)
     distance[source_idx] = 0.0
-    status = np.zeros(N, dtype=np.int8)  # 0=FAR, 1=TRIAL, 2=KNOWN
 
-    heap = []
+    # Initialize status of each node along the front
+    status = np.zeros(N, dtype=np.int8)  # 0=FAR, 1=TRIAL, 2=KNOWN
     status[source_idx] = 2
 
+    heap = []
+
+    # Add vertices adjacent to source node
     for v in mesh.vertex_neighbors[source_idx]:
         d = np.linalg.norm(V[v] - V[source_idx])
         distance[v] = d
         status[v] = 1
         heapq.heappush(heap, (d, v))
 
-    vertex_faces = [[] for _ in range(N)]
-    for i, f in enumerate(F):
-        for v in f:
-            vertex_faces[v].append(i)
-
     while heap:
+
+        # Update front status
         d_min, v = heapq.heappop(heap)
         if status[v] == 2:
             continue
         status[v] = 2
 
-        for f_idx in vertex_faces[v]:
+        for f_idx in VERTEX_TO_FACES[v]:
             tri = F[f_idx]
             known = [u for u in tri if status[u] == 2]
             unknown = [u for u in tri if status[u] != 2]
 
+            # We require at least two knowns to compute an unknown
             if len(unknown) != 1 or len(known) < 2:
                 continue
             
@@ -87,8 +100,10 @@ def fast_marching(mesh, source_idx):
             C = V[u]
             dA, dB = distance[known[0]], distance[known[1]]
 
+            # Compute distance to third vertex 
             d_new = eikonal_update_triangle(A, B, C, dA, dB)
-            
+
+            # Accept smaller distance
             if d_new < distance[u]:
                 distance[u] = d_new
                 heapq.heappush(heap, (d_new, u))
--- main.py
+++ main.py
@@ -7,53 +7,53 @@ import viser
 import viser.transforms as tf
 import numpy as np
 
+# Mesh setup
 MESH_FOLDER = "meshes"
-MESHES = ["bunny", "happy"]
-MESH_SCALE_FACTOR = 2
+MESHES = ["bunny"]
+MESH_SCALE_FACTOR = 20
 
 SERVER = None
 MESH = None
+DISTANCES = None
 
+# Size of source and sink point
 POINT_SCALE_FACTOR = 0.005
 
+# Rotation matrices
 T_WXYZ = tf.SO3.from_x_radians(np.pi/2).wxyz
 T_MATRIX = tf.SO3.from_x_radians(np.pi/2).as_matrix()
 
+# General GUI information
 class GUI:
     def __init__(self):
         self.source_num = None
-        self.sink_num = None
+        self.source_idx = None
         self.source_btn = None
+        self.sink_num = None
+        self.sink_idx = None
         self.sink_btn = None
 
 GUI = GUI()
 
+# Create Viser scene
 def init_scene():
-    global SERVER
+    global SERVER, DISTANCES
 
-    source_idx = 0
+    # Initialize to first and last vertex for source and sink
+    GUI.source_idx = source_idx = 0
     source_pos_world = T_MATRIX @ MESH.vertices[source_idx]
 
-    sink_idx = len(MESH.vertices) - 1
+    GUI.sink_idx = sink_idx = len(MESH.vertices) - 1
     sink_pos_world = T_MATRIX @ MESH.vertices[sink_idx]
-    # Create a small red sphere at source position
-    source_sphere = SERVER.scene.add_icosphere(
-        name="/source_marker",
-        radius=np.linalg.norm(MESH.bounding_box.extents)*POINT_SCALE_FACTOR,
-        color=(0, 150, 0),
-        position=tuple(source_pos_world),    # initial position
-    )
 
-    sink_sphere = SERVER.scene.add_icosphere(
-        name="/sink_marker",
-        radius=np.linalg.norm(MESH.bounding_box.extents)*POINT_SCALE_FACTOR,
-        color=(0, 150, 0),
-        position=tuple(sink_pos_world),    # initial position
-    )
 
-    distances = fast_marching(MESH, source_idx)
-    MESH.visual.vertex_colors = distance_to_colors(distances)
+    # Compute distance field
+    DISTANCES = fast_marching(MESH, source_idx)
 
+    # Compute color according to distance field
+    MESH.visual.vertex_colors = distance_to_colors(DISTANCES)
+
+    # Add mesh
     SERVER.scene.add_mesh_trimesh(
         "/mesh",
         mesh = MESH,
@@ -61,77 +61,150 @@ def init_scene():
         position=(0,0,0),
     )
 
-    path = trace_geodesic(MESH, distances, source_idx, sink_idx)
-    path = path @ T_MATRIX.T
-    path = np.stack([path[:-1], path[1:]], axis=1)
+    # Add level sets
+    contours = trace_contours(MESH, DISTANCES)
+    contours = contours @ T_MATRIX.T
     SERVER.scene.add_line_segments(
-        "/gradient",
+        "/contours",
+        points = contours,
+        colors = (0, 0, 0),
+    )
+
+    # Add geodesic path
+    path = trace_geodesic(MESH, DISTANCES, source_idx, sink_idx)
+    path = path @ T_MATRIX.T
+    SERVER.scene.add_line_segments(
+        "/geodesic",
         points = path,
         colors = (0, 150, 0),
         line_width = 3.0
     )
 
-
-    
-def update_scene(event):
-    global SERVER
-
-    source_idx = int(GUI.source_num.value)
-    source_pos_world = T_MATRIX @ MESH.vertices[source_idx]
-
-    sink_idx = int(GUI.sink_num.value)
-    sink_pos_world = T_MATRIX @ MESH.vertices[sink_idx]
-    # Create a small red sphere at source position
-    source_sphere = SERVER.scene.add_icosphere(
+    # Add points at source and sink
+    SERVER.scene.add_icosphere(
         name="/source_marker",
         radius=np.linalg.norm(MESH.bounding_box.extents)*POINT_SCALE_FACTOR,
         color=(0, 150, 0),
-        position=tuple(source_pos_world),    # initial position
+        position=tuple(source_pos_world),
     )
 
-    sink_sphere = SERVER.scene.add_icosphere(
+    SERVER.scene.add_icosphere(
         name="/sink_marker",
         radius=np.linalg.norm(MESH.bounding_box.extents)*POINT_SCALE_FACTOR,
         color=(0, 150, 0),
-        position=tuple(sink_pos_world),    # initial position
-    )
-    distances = fast_marching(MESH, source_idx)
-    MESH.visual.vertex_colors = distance_to_colors(distances)
-
-    SERVER.scene.add_mesh_trimesh(
-        "/mesh",
-        mesh=MESH,
-        wxyz=T_WXYZ,
-        position=(0,0,0),
-        cast_shadow = False,
-        receive_shadow = False,
-        flat_shading=False,
-        material=viser.MeshUnlitMaterial()
+        position=tuple(sink_pos_world), 
     )
 
+# Update scene on Viser event
+def update(event):
+    global SERVER, DISTANCES
+
+    source_not_updated = GUI.source_idx == GUI.source_num.value
+
+    # Update source and sink
+    GUI.source_idx = source_idx = int(GUI.source_num.value)
+    source_pos_world = T_MATRIX @ MESH.vertices[source_idx]
+
+    GUI.sink_idx = sink_idx = int(GUI.sink_num.value)
+    sink_pos_world = T_MATRIX @ MESH.vertices[sink_idx]
+    
+    # Check if only sink was updated
+    # if so, we only need to move the geodesic
+    if (source_not_updated):
+        path = trace_geodesic(MESH, DISTANCES, source_idx, sink_idx)
+        path = path @ T_MATRIX.T
+
+        # Remove old element
+        SERVER.scene.remove_by_name("/geodesic")
+
+        # Add new element
+        SERVER.scene.add_line_segments(
+            "/geodesic",
+            points = path,
+            colors = (0, 150, 0),
+            line_width = 3.0
+        )
+    else: 
+        # Recalculate all fields
+        DISTANCES = fast_marching(MESH, source_idx)
+        contours = trace_contours(MESH, DISTANCES)
+        contours = contours @ T_MATRIX.T
+        path = trace_geodesic(MESH, DISTANCES, source_idx, sink_idx)
+        path = path @ T_MATRIX.T
+
+        # Compute color according to distance field
+        MESH.visual.vertex_colors = distance_to_colors(DISTANCES)
+
+        # Remove old elements
+        SERVER.scene.remove_by_name("/mesh")
+        SERVER.scene.remove_by_name("/contours")
+        SERVER.scene.remove_by_name("/geodesic")
+        SERVER.scene.remove_by_name("/source_marker")
+        SERVER.scene.remove_by_name("/sink_marker")
+
+        # Add in new elements
+        SERVER.scene.add_mesh_trimesh(
+            "/mesh",
+            mesh = MESH,
+            wxyz=T_WXYZ,
+            position=(0,0,0),
+        )
+
+        SERVER.scene.add_line_segments(
+            "/contours",
+            points = contours,
+            colors = (0, 0, 0),
+        )
+
+        SERVER.scene.add_line_segments(
+            "/geodesic",
+            points = path,
+            colors = (0, 150, 0),
+            line_width = 3.0
+        )
+
+        SERVER.scene.add_icosphere(
+            name="/source_marker",
+            radius=np.linalg.norm(MESH.bounding_box.extents)*POINT_SCALE_FACTOR,
+            color=(0, 150, 0),
+            position=tuple(source_pos_world),
+        )
+
+        SERVER.scene.add_icosphere(
+            name="/sink_marker",
+            radius=np.linalg.norm(MESH.bounding_box.extents)*POINT_SCALE_FACTOR,
+            color=(0, 150, 0),
+            position=tuple(sink_pos_world), 
+        )
+
+
+# Create Viser GUI
 def init_gui():
     global GUI
+
+    # Create field for changing source vertex
     GUI.source_num = SERVER.gui.add_number(
-    "Source Index",
-    min=0,
-    max=len(MESH.vertices) - 1,
-    step=1,
-    initial_value=0,
+        "Source Index",
+        min=0,
+        max=len(MESH.vertices) - 1,
+        step=1,
+        initial_value=0,
     )
 
-
+    #Create button to instantiate change
     GUI.source_btn = SERVER.gui.add_button(
         "Update",
     )
 
+    # Create field for changing sink vertex    
     GUI.sink_num = SERVER.gui.add_number(
-    "Destination Index",
-    min=0,
-    max=len(MESH.vertices) - 1,
-    step=1,
-    initial_value=len(MESH.vertices) - 1,
+        "Destination Index",
+        min=0,
+        max=len(MESH.vertices) - 1,
+        step=1,
+        initial_value=len(MESH.vertices) - 1,
     )
-
+    #Create button to instantiate change
     GUI.sink_btn = SERVER.gui.add_button(
         "Update",
     )
@@ -139,29 +212,40 @@ def init_gui():
     GUI.source_btn.on_click(update)
     GUI.sink_btn.on_click(update)
 
+# Create Viser server
 def init():
     global SERVER, MESH
 
+    # Load in first mesh in MESHES
+    # ensuring we only consider a connected component
     MESH = trimesh.load(MESH_FOLDER + "/" +  MESHES[0] + ".obj").split(only_watertight=False)[0]
+
+    # Translate and rotate mesh so it is upright
+    # and centroid is at origin
+    centroid = MESH.centroid
+    MESH.vertices -= centroid
     MESH.apply_scale(MESH_SCALE_FACTOR)
+
+    # Precompute adjacencies
     build_adjacency(MESH)
-    
+
+    # Create server
     SERVER = viser.ViserServer()
     SERVER.scene.enable_default_lights(enabled=False)
+
+    # Initialize GUI and scene
     init_gui()
     init_scene()
 
-def update(event):
-    update_scene(event)
-
 def main():
-    MESH = trimesh.load(MESH_FOLDER + "/" +  MESHES[0] + ".obj").split(only_watertight=False)[0]
+
+    # Create server
     init()
 
     # Keep server alive
     try:
         while True:
-            time.sleep(1.0)
+            continue
     except KeyboardInterrupt:
         print("Server stopped")
 
